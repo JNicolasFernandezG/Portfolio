a4c10cc5ce8bfddaf01656511d875bbb1e9b99b9
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-10-31 18:07:59 -0300

    paso 2

--- portfolio/src/components/sobreMi/Tarjetas.tsx
+++ portfolio/src/components/sobreMi/Tarjetas.tsx
@@ -3,34 +3,48 @@ import type React from "react";
 
 //styles
 import styles from"./Tarjetas.module.css";
-import { body } from "framer-motion/client";
 
+//utils
+import { social_links } from "./utils";
 
 export const SobremiComponent: React.FC = () => {
     return (
-        <body>
+        <section className={styles['sobremi-section']}>
             <h2>
                 Desarrollador Full Stack | Del Liderazgo a la Innovacion en Código
             </h2>
             <div className={styles['cards']}>
-                <>
+                
+                <div className={styles['description-contact-column']}>
                 <p className={styles['card-description']}>
-                Como Desarrollador Full Stack, combino habilidades técnicas y liderazgo para crear soluciones innovadoras. Mi experiencia en gestión de equipos y proyectos me permite entregar resultados de alta calidad, optimizando procesos y fomentando la colaboración. Apasionado por la tecnología, busco constantemente nuevas formas de mejorar y adaptarme a las tendencias del sector.
+                Desarrollador Full Stack certificado, especializado en crear sistemas robustos y eficientes utilizando TypeScript, Next.js, NestJS y PostgreSQL. Mi experiencia no se limita al código; mi pasado como líder de equipos me ha enseñado a gestionar proyectos, verificar tareas y resolver problemas complejos con una visión integral.
                 </p>
                 <button className={styles['card-button']}>
                     LEER MAS
                 </button>
-                </>
 
-                <>
+                <div className={styles['contact-card']}>
                 <h3 className={styles['contact-title']}>Contactemos Creando el Futuro, juntos</h3>
                 <p className={styles['contact-info']}> 
-                Email:
-                Linkedin:
-                Github:
-                Twitter:
+                    
+                    <a href={`mailto:${social_links.email}`} target="_blank" className={styles["email"]} rel=" noopener noreferrer">
+                    nicolas.fernandezGamarra@gmail.com
+                </a>
+                    <a href={social_links.linkedin} target="_blank" className={styles["social-icon"]} rel="noopener noreferrer">
+                    LINKEDIN
+                </a>
+                <a href={social_links.twitter} target="_blank" className={styles["social-icon"]} rel="noopener noreferrer">
+                    TWITTER
+                </a>
+                <a href={social_links.github} target="_blank" className={styles["social-icon"]} rel="noopener noreferrer">
+                    GITHUB
+                </a>
+
                 </p>
-                </>
+                
+                </div>
+
+                </div>
 
                 <>
                 <p className={styles['aptitudes']}> 
@@ -44,7 +58,7 @@ export const SobremiComponent: React.FC = () => {
                 </p>
                 </>
             </div>
-        </body>
+        </section>
     )
 
 }
---
+++ portfolio/src/components/sobreMi/utils.tsx
@@ -0,0 +1,6 @@
+export const social_links = {
+    linkedin: 'https://www.linkedin.com/in/jorge-nicolas-fernandez-gamarra',
+    twitter: 'https://x.com/NicFernandezG',
+    github: 'https://github.com/JNicolasFernandezG',
+    email: 'nicolas.fernandezgamarra@gmail.com'
+}
