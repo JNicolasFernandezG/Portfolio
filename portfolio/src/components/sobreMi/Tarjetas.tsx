05f1079c5b479a89e9e3315416970db80705163e
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-11-03 20:43:58 -0300

    fin por hoy

--- portfolio/src/components/sobreMi/Tarjetas.tsx
+++ portfolio/src/components/sobreMi/Tarjetas.tsx
@@ -11,13 +11,23 @@ export const SobremiComponent: React.FC = () => {
     return (
         <section className={styles['sobremi-section']}>
             <h2>
-                Desarrollador Full Stack | Del Liderazgo a la Innovacion en Código
+                Desarrollador&nbsp;
+                <span className={styles['highlight-orange']}>Full Stack</span>&nbsp;
+                | Del&nbsp;
+                <span className={styles['highlight-orange']}>Liderazgo</span>&nbsp;
+                a la&nbsp;
+                <span className={styles['highlight-orange']}>Innovacion</span>&nbsp;
+                en&nbsp;
+                <span className={styles['highlight-orange']}>Código</span>
             </h2>
             <div className={styles['cards']}>
                 
                 <div className={styles['card-description']}>
                 <p className={styles['description']}>
-                Desarrollador Full Stack certificado, especializado en crear sistemas robustos y eficientes utilizando TypeScript, Next.js, NestJS y PostgreSQL. Mi experiencia no se limita al código; mi pasado como líder de equipos me ha enseñado a gestionar proyectos, verificar tareas y resolver problemas complejos con una visión integral.
+                Desarrollador Full Stack certificado,&nbsp;
+                <span className={styles['highlight-orange']}>especializado en crear sistemas robustos y eficientes utilizando TypeScript, Next.js, NestJS y PostgreSQL.</span>&nbsp;
+                Mi experiencia no se limita al código; mi pasado como líder de equipos me ha enseñado a gestionar proyectos, verificar tareas y&nbsp;
+                <span className={styles['highlight-orange']}>resolver problemas complejos con una visión integral.</span>
                 </p>
                 <button className={styles['card-button']}>
                     LEER MAS
@@ -31,19 +41,20 @@ export const SobremiComponent: React.FC = () => {
                     "Superando desafíos con persistencia."
                     </p>
                 </div>
+                
                 <div className={styles['contact-card']}>
                 <h3 className={styles['contact-title']}>Contactemos Creando el Futuro, juntos</h3>
 
-                    <a href={`mailto:${social_links.email}`} target="_blank" className={styles["email"]} rel=" noopener noreferrer">
+                    <a href={`mailto:${social_links.email}`} target="_blank" className={styles["contact-text"]} rel=" noopener noreferrer">
                     nicolas.fernandezGamarra@gmail.com
                 </a>
-                    <a href={social_links.linkedin} target="_blank" className={styles["social-icon"]} rel="noopener noreferrer">
+                    <a href={social_links.linkedin} target="_blank" className={styles["contact-text"]} rel="noopener noreferrer">
                     LINKEDIN
                 </a>
-                <a href={social_links.twitter} target="_blank" className={styles["social-icon"]} rel="noopener noreferrer">
+                <a href={social_links.twitter} target="_blank" className={styles["contact-text"]} rel="noopener noreferrer">
                     TWITTER
                 </a>
-                <a href={social_links.github} target="_blank" className={styles["social-icon"]} rel="noopener noreferrer">
+                <a href={social_links.github} target="_blank" className={styles["contact-text"]} rel="noopener noreferrer">
                     GITHUB
                 </a>
                 </div>
