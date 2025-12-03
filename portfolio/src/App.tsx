88ce28c8acc98231f1385911add5d0b6c4ae5e0f
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-12-03 17:29:10 -0300

    se arranco con la 3er parte

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,6 +1,8 @@
 
 import PortadaComponent from './components/Header/Portada/portada';
 import SobremiComponent from './components/sobreMi/Tarjetas';
+import ProyectosComponent from './components/proyectos/proyectos';
+
 import './index.css';
 
 function App() {
@@ -9,6 +11,7 @@ function App() {
     <main className='scroll-snap-container'>
       <PortadaComponent/>
       <SobremiComponent/>
+      <ProyectosComponent/>
     </main>
 
   )
---
+++ portfolio/src/components/proyectos/proyectos.tsx
@@ -0,0 +1,29 @@
+// vendors
+import type React from "react";
+import { Reveal } from "react-awesome-reveal"; 
+
+// styles
+import styles from "./proyectos.module.css";
+
+// utils
+
+export const proyextosComponent: React.FC = () => {
+    return (
+        <section className={styles['proyectos-section']}>
+            <div>
+                
+            </div>
+
+
+        </section>
+    )
+
+}
+
+
+
+
+export default proyextosComponent;
+
+
+
