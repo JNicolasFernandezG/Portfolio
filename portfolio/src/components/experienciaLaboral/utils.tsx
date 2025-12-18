f31ccaa5665007479400327fb0f78503b70a14b0
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-12-18 20:12:21 -0300

    falta mejorar educacion

--- portfolio/src/components/experienciaLaboral/utils.tsx
+++ portfolio/src/components/experienciaLaboral/utils.tsx
@@ -6,21 +6,8 @@ const Highlight = ({ children }: { children: React.ReactNode }) => (
     <span className="highlight-orange">{children}</span>
 );
 
+
 const titulo1 = (
-    <>
-    Técnico de Soporte | Independiente
-    <br />
-    Feb 2017 - Oct 2019
-    </>
-);
-
-const desc1 = (
-    <>
-    Logro Principal: <Highlight> Diagnóstico y reparación integral de hardware y software. </Highlight> Brindé soluciones técnicas personalizadas y mantuve una alta satisfacción del cliente, enfocándome en la optimización del rendimiento de los equipos
-    </>
-);
-
-const titulo2 = (
     <>
     Oficial de Servicio | Ministerio de Seguridad
     <br />
@@ -28,9 +15,23 @@ const titulo2 = (
     </>
 );
 
-const desc2 = (
+const desc1 = (
     <>
     Logro Principal: <Highlight> Liderazgo en la resolución de problemas complejos y gestión de equipos a gran escala. </Highlight> Tomé decisiones estratégicas en entornos de alta presión para asegurar la eficiencia y el cumplimiento de objetivos.
+    </>
+);
+
+const titulo2 = (
+    <>
+    Técnico de Soporte | Independiente
+    <br />
+    Feb 2017 - Oct 2019
+    </>
+);
+
+const desc2 = (
+    <>
+    Logro Principal: <Highlight> Diagnóstico y reparación integral de hardware y software. </Highlight> Brindé soluciones técnicas personalizadas y mantuve una alta satisfacción del cliente, enfocándome en la optimización del rendimiento de los equipos
     </>
 );
 
