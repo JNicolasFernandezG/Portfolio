9a4aed150f6a3087641c8c67272f155af0eaf5b8
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-12-22 12:25:49 -0300

    avance 1

---
+++ portfolio/src/components/educacion/educacion.tsx
@@ -0,0 +1,18 @@
+//vendors
+
+import React from 'react';
+import { Reveal } from 'react-awesome-reveal';
+
+//styles
+import styles from './educacion.module.css';
+
+//
+
+
+export const EducacionComponent: React.FC = () => {
+    return (
+        <section className={styles['educacion-section']}>
+        
+        </section>
+    )
+}
---
+++ portfolio/src/components/educacion/utils.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import "@/index.css";
+import { type Educacion } from "./Educacion";
+
+const titulo1 = (
+    <></>
+);
+
+const desc1 = (
+    <></>
+);   
+
+const titulo2 = (
+    <></>
+);
+
+const desc2 = (
+    <></>
+);
+
+const titulo3 = (
+    <></>
+);
+
+const desc3 = (
+    <></>
+);
+
+const titulo4 = (
+    <></>
+);
+
+const desc4 = (
+    <></>
+);
+
+const titulo5 = (
+    <></>
+);  
+
+const desc5 = (
+    <></>
+);  
+
+const titulo6 = (
+    <></>
+);
+
+const desc6 = (
+    <></>
+);  
+
+
+export const EducacionUtils: Educacion[] = [
+    {id: 1, title: titulo1, description: desc1},
+    {id: 2, title: titulo2, description: desc2},
+    {id: 3, title: titulo3, description: desc3},
+    {id: 4, title: titulo4, description: desc4},
+    {id: 5, title: titulo5, description: desc5},
+    {id: 6, title: titulo6, description: desc6},
+];
