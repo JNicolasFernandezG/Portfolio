a956ca15d2e4fb04b679727be6cf0f0b2aa4b009
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-12-22 21:30:16 -0300

    cambios realizados falta modificar css y colocar resto de reveal

---
+++ portfolio/src/components/educacion/miEducacion.tsx
@@ -0,0 +1,58 @@
+//vendors
+
+import React from 'react';
+import { Reveal } from 'react-awesome-reveal';
+
+//styles
+import styles from './educacion.module.css';
+
+//utils
+import { EducacionUtils } from './utils';
+
+import { useEducacion } from './useEducacion';
+
+import {
+    ANIMATION_DURATION, 
+    TRIGGER_ONCE,
+    titleSlideInCentered, 
+    cardFadeInUp,
+    CASCADE_DAMPING,
+} from "@/config/animaciones.ts"
+import { div } from 'framer-motion/client';
+
+
+export const EducacionComponent: React.FC = () => {
+    const {educacionVisibles, cambiarPagina, esUltimaPagina, pagina} = useEducacion(3);
+    
+    return (
+        <section className={styles['educacion-section']}>
+            <Reveal
+                keyframes={titleSlideInCentered}
+                duration={ANIMATION_DURATION}
+                triggerOnce={TRIGGER_ONCE}
+            >    
+                <h2 className={styles['titulo']}>
+                    EDUCACIÒN
+                </h2>
+            </Reveal>
+            <div className={styles['cards']}>
+                {educacionVisibles.map((edu)=>(
+                    <div key={edu.id} className={styles[`educacion-card`]}>
+                        <h3>{edu.title}</h3>
+                        <p>{edu.description}</p>
+                    </div>
+                ))}
+            
+            </div>
+            <div className={styles['container-boton']}>
+                <button 
+                    className={styles['boton-cambiar-pagina']} onClick={cambiarPagina}>
+                    {esUltimaPagina ? 'Volver al inicio' : 'Ver más'}
+                </button>
+            </div>
+
+        </section>
+    );
+}
+
+export default EducacionComponent;
