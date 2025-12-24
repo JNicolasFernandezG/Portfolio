62366873e5034669c4fa95ad898d1867a395f010
Jorge Nicolas Fernandez Gamarra <nicolas.fernandezgamarra@gmail.com>
2025-12-23 22:04:27 -0300

    falta seguir actualizando habilidades continuar mañana

---
+++ portfolio/src/components/herramientas/utils.tsx
@@ -0,0 +1,35 @@
+
+
+export const topTools = [
+    {icon: "/icon/CLOUDINARY.png"},
+    {icon: "/icon/JS.png" },
+    {icon: "/icon/TS.png" },
+    {icon: "/icon/REACT.png" },
+    {icon: "/icon/NEST.png" },
+    {icon: "/icon/VITE.png"},
+    {icon: "/icon/REDUX.png"},
+    {icon: "/icon/SCRUM.png"},
+    {icon: "/icon/NODE.png"},
+];
+
+export const bottomTools = [
+    {icon: "/icon/EXPRESS.png"},
+    {icon: "/icon/POSTGRESQL.png" },
+    {icon: "/icon/MONGODB.png" },
+    {icon: "/icon/TYPEORM.png" },
+    {icon: "/icon/DOCKER.png" },
+    {icon: "/icon/GIT.png"},
+    {icon: "/icon/POSTMAN.png"},
+    {icon: "/icon/JEST.png"},
+    {icon: "/icon/SWAGGER.png"},
+];
+
+export const allToolsNames = [
+    "JavaScript", "TypeScript", "React", "Next.js", "Redux", "Vite", 
+    "Node.js", "NestJS", "Express.js", "PostgreSQL", "MongoDB", 
+    "TypeORM", "Docker", "Git", "Jest", "Postman", "Swagger", 
+    "Cloudinary", "Scrum", "CORS", "JWT", "Bcrypt",
+    "React Router DOM", "ts-node", "Passport.js", "Supertest",
+    "Nodemailer", "Google Auth Library", "Cookie-Parser", "ESLint",
+    "Yarn", "Nodemon", "npm", "tsconfig-paths", "Prettier"
+].sort();
